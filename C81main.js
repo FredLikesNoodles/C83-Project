f089e162da203f623fd698a289c51de13e8e32e2
FredLikesNoodles <82772444+FredLikesNoodles@users.noreply.github.com>
2021-09-13 17:23:05 -0700

    Add files via upload

--- C81main.js
+++ C81main.js
@@ -13,7 +13,38 @@ canvas = document.getElementById("mycanvas");
  document.body.style.overflow="hidden";
 
  }
-
+ var mouseEvent = "empty";
+ var lpx, lpy;
+ canvas.addEventListener("mousedown", my_mousedown);
+ function my_mousedown(e){
+    mouseEvent = "mouseDown";
+ }
+ canvas.addEventListener("mouseup", my_mouseup);
+ function my_mouseup(e){
+     mouseEvent = "mouseUp";
+ }
+ canvas.addEventListener("mouseleave", my_mouseleave);
+ function my_mouseleave(e){
+     mouseEvent = "mouseLeave";
+ }
+ canvas.addEventListener("mousemove", my_mousemove);
+ function my_mousemove(e){
+    color = document.getElementById("color_input").value;
+    width = document.getElementById("width_input").value;
+    cpx = e.clientX - canvas.offsetLeft;
+     cpy = e.clientY - canvas.offsetTop;
+     
+     if(mouseEvent == "mouseDown"){
+         ctx.beginPath();
+         ctx.strokeStyle = color;
+         ctx.lineWidth = width;
+         ctx.moveTo(lpx, lpy);
+         ctx.lineTo(cpx, cpy);
+         ctx.stroke();
+     }
+     lpx = cpx;
+     lpy = cpy;
+ }
  canvas.addEventListener("touchstart", my_touchstart);
  function my_touchstart(e){
     color = document.getElementById("color_input").value;
